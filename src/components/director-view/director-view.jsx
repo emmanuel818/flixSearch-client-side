e0dd51400289f552537067c8a30cee00ea8a18a0
emmanuel818 <emmanuelg8182001@gmail.com>
2021-12-28 11:46:03 -0600

    Updated styles using react bootstrap

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Container, Row, Col, Card, Button, CardGroup } from 'react-bootstrap';
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
+import './director-view.scss';
 
 export function DirectorView({ director, onBackClick }) {
   return (
     <Container>
       <Row>
         <Col></Col>
-        <Card border='primary' style={{ marginTop: 50, marginBottom: 30, height: 300, width: 500 }}>
-          <CardGroup>
+        <Card className="director-card" border='primary' style={{ marginTop: 50, marginBottom: 30, height: 260, width: 500 }}>
+          <Card.Body>
             <Card.Title>{director.Name}</Card.Title>
             <Card.Text>{director.Bio}</Card.Text>
             <Button onClick={() => onBackClick()}>Back</Button>
-          </CardGroup>
+          </Card.Body>
         </Card>
         <Col></Col>
       </Row>
     </Container>
-
   )
 }
 
